b77c4cd44a4568c3590006bee9db5d7e22e9a61b
Brousse.M <mbweb47@gmail.com>
2020-08-18 01:09:04 +0200

    index -> login

--- src/Security/AppAuthenticator.php
+++ src/Security/AppAuthenticator.php
@@ -98,7 +98,7 @@ class AppAuthenticator extends AbstractFormLoginAuthenticator implements Passwor
 
         // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
         // throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
-        return new RedirectResponse($this->urlGenerator->generate('easyadmin'));
+        return new RedirectResponse($this->urlGenerator->generate('admin'));
     }
 
     protected function getLoginUrl()
